cf17f80a83cb0ca39fed0eae0f7bc3ed92da08c2
Yannick Weiss <hey@yannick.dev>
2022-04-03 19:00:35 +0200

    Initial Commit

---
+++ BeaconExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeaconExample
+//
+//  Created by Yannick on 03.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
